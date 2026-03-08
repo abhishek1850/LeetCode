5bb3ea76f8f60b657ef844c252d9f649704a98bb
Purohit Abhishek Babusingh <purohitabhishek1850@gmail.com>
2026-03-08 08:42:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
+      // int time = 0;
+      // while(tickets[k]!=0){
+      //   for(int i = 0;i<tickets.size();i++){
+      //     if(tickets[k]==0){
+      //       break;
+      //     }
+      //     if(tickets[i]>0){
+      //     time++;
+      //     tickets[i]--;
+      //   }
+      //   }
+      // }
+      //   return time;
+
+
       int time = 0;
-      while(tickets[k]!=0){
-        for(int i = 0;i<tickets.size() && tickets[k]!=0;i++){
-          if(tickets[k]==0){
-            break;
+      int n = tickets.size();
+      for(int i = 0 ;i<n;i++){
+        if(i<k){
+          if(tickets[i]<=tickets[k]){
+            time+=tickets[i];
           }
-          if(tickets[i]>0){
-          time++;
-          tickets[i]--;
+          else{
+            time+=tickets[k];
+          }
         }
+
+        else if(i==k){
+          time+=tickets[k];
+        }
+        else{
+
+          if(tickets[i]<tickets[k]){
+            time+=tickets[i];
+          }
+          else{
+            time+=tickets[k]-1;
+          }
         }
       }
-        return time;
+      return time;
     }
 };
