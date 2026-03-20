0fce98f122ddb8b62040ba98338bf9a22b818ee9
Purohit Abhishek Babusingh <purohitabhishek1850@gmail.com>
2026-03-20 15:39:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -12,25 +12,41 @@
 class Solution {
 public:
 
-    int height(TreeNode* root){
-      if(root==NULL){
-        return 0;
-      }
-      int left = height(root->left);
-      int right = height(root->right);
-      int curr = max(left,right)+1;
-      return curr;
-    }
+    // int height(TreeNode* root){
+    //   if(root==NULL){
+    //     return 0;
+    //   }
+    //   int left = height(root->left);
+    //   int right = height(root->right);
+    //   int curr = max(left,right)+1;
+    //   return curr;
+    // }
 
 
-    int diameterOfBinaryTree(TreeNode* root) {
-      if(root==NULL){
-        return 0;
-      }
-      int curr = height(root->left)+height(root->right);
-      int left = diameterOfBinaryTree(root->left);
-      int right = diameterOfBinaryTree(root->right);
-      return max(curr,max(left,right));
+    // int diameterOfBinaryTree(TreeNode* root) {
+    //   if(root==NULL){
+    //     return 0;
+    //   }
+    //   int curr = height(root->left)+height(root->right);
+    //   int left = diameterOfBinaryTree(root->left);
+    //   int right = diameterOfBinaryTree(root->right);
+    //   return max(curr,max(left,right));
         
+    // }
+
+    pair<int,int>diameter(TreeNode* root){
+      if(root==NULL){
+        return {0,0};
+      }
+
+      pair<int,int>leftInfo = diameter(root->left);
+      pair<int,int>rightInfo = diameter(root->right);
+      int currD = leftInfo.second + rightInfo.second ;
+      int finalD = max(currD, max(leftInfo.first,rightInfo.first));
+      int finalH = max(leftInfo.second,rightInfo.second)+1;
+      return make_pair(finalD,finalH);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+      return diameter(root).first;
     }
 };
