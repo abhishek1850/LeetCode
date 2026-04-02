3de1e04d2448dfbc4efe4c07198b014a425ec6ac
Purohit Abhishek Babusingh <purohitabhishek1850@gmail.com>
2026-04-02 20:04:50 +0530

    Time: 0 ms (100%), Space: 64.9 MB (91.95%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.cpp
@@ -15,12 +15,25 @@ public:
       if(root==NULL ){
         return 0;
       }
-      int left = rangeSumBST(root->left,low,high);
-      int right = rangeSumBST(root->right,low,high);
+      // int left = rangeSumBST(root->left,low,high);
+      // int right = rangeSumBST(root->right,low,high);
+
+      // if(root->val>=low && root->val<=high){
+      //   return left+right+root->val;
+      // }
+      // return left+right; 
 
       if(root->val>=low && root->val<=high){
-        return left+right+root->val;
+        int left = rangeSumBST(root->left,low,high);
+        int right =rangeSumBST(root->right,low,high);
+        return root->val+left+right;
       }
-      return left+right; 
+      else if(root->val<=low){
+        return rangeSumBST(root->right,low,high);
+      }
+      else if(root->val>=high){
+        return rangeSumBST(root->left,low,high);
+      }
+      return root->val;
     }
 };
