ecd3cf59b622c19cbc2d4fba889e72bce4086d19
Purohit Abhishek Babusingh <purohitabhishek1850@gmail.com>
2026-06-10 14:08:24 +0530

    Time: 171 ms (7.46%), Space: 84.1 MB (15.19%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,35 +1,50 @@
-class Points{
-  public:
-  int idx;
-  int dist;
-  vector<int>s;
+// class Points{
+//   public:
+  
+//   int dist;
+//   vector<int>s;
 
-  Points(int i , int d, vector<int> ss){
-    idx = i;
-    dist = d;
-    s = ss;
-  }
-
-
-  bool operator < (const Points& obj) const {
-    return this->dist > obj.dist;
-  }
-};
+//   Points(int d, vector<int> ss){
+//     dist = d;
+//     s = ss;
+//   }
+//   bool operator < (const Points& obj) const {
+//     return this->dist > obj.dist;
+//   }
+// };
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
 
-      vector<Points>store;
+      // vector<Points>store;
+
+
+      priority_queue<
+      pair<int,vector<int>>,
+      vector<pair<int,vector<int>>>,
+      greater<pair<int,vector<int>>>
+      >pq;
       
-      for(int i = 0;i<points.size();i++){
-        int dist = (points[i][0]*points[i][0])+(points[i][1]*points[i][1]);
-        store.push_back(Points(i,dist,points[i]));
+      // for(int i = 0;i<points.size();i++){
+      //   int dist = (points[i][0]*points[i][0])+(points[i][1]*points[i][1]);
+      //   store.push_back(Points(dist,points[i]));
+      // }
+
+      for(auto & p : points){
+        int dist = p[0]*p[0]+p[1]*p[1];
+        pq.push({dist,p});        
       }
 
-      priority_queue<Points>pq(store.begin(),store.end());
+
+      // priority_queue<Points>pq(store.begin(),store.end());
       vector<vector<int>>ans;
+      // for(int i = 0;i<k;i++){
+      //   ans.push_back(pq.top().s);
+      //   pq.pop();
+      // }
+
       for(int i = 0;i<k;i++){
-        ans.push_back(pq.top().s);
+        ans.push_back(pq.top().second);
         pq.pop();
       }
         
