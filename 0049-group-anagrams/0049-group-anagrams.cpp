8cc4f7170574d3859e596383b175f4d7f8c319e7
Purohit Abhishek <purohitabhishek1850@gmail.com>
2026-08-25 12:12:50 +0530

    Time: 76 ms (6.33%), Space: 34.5 MB (7.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,25 +1,41 @@
 class Solution {
 
-  bool count(vector<int>&s,string str){
-    for(auto i : str){
-      s[i-'a']++;
+    void count(vector<int>& s, string str) {
+        for (auto i : str) {
+            s[i - 'a']++;
+        }
     }
-    return true;
-  }
+
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-      map<vector<int>,vector<string>>mp; 
-      for(auto i:strs){
-        vector<int>freq(26,0);
-        count(freq,i);
-        mp[freq].push_back(i);
-      }
+        // map<vector<int>,vector<string>>mp;
+        // for(auto i:strs){
+        //   vector<int>freq(26,0);
+        //   count(freq,i);
+        //   mp[freq].push_back(i);
+        // }
 
-      vector<vector<string>>ans;
-      for(auto i:mp){
-        ans.push_back(i.second);
-      }
-      return ans;
-        
+        // vector<vector<string>>ans;
+        // for(auto i:mp){
+        //   ans.push_back(i.second);
+        // }
+        // return ans;
+        unordered_map<string, vector<string>> mp;
+        for (auto s : strs) {
+            vector<int> freq(26, 0);
+            count(freq, s);
+
+            string key = "";
+            for (int i : freq) {
+                key += to_string(i) + "#";
+            }
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto& i : mp) {
+            ans.push_back(i.second);
+        }
+        return ans;
     }
 };
