428e10880b57184101cf0ff09f75a0b31fcba6a0
Purohit Abhishek <purohitabhishek1850@gmail.com>
2026-09-18 10:03:23 +0530

    Time: 0 ms (100%), Space: 14.1 MB (57.5%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  void solve(vector<int>& candidates, int target, vector<vector<int>>&ans,vector<int>&store,int sum,int index){
+    if(sum==target){
+      ans.push_back(store);
+      return;
+    }
+    if(sum>target){
+      return ;
+    }
+
+    for(int i =index;i<candidates.size();i++){
+      sum+=candidates[i];
+      store.push_back(candidates[i]);
+      solve(candidates,target,ans,store,sum,i);
+      store.pop_back();
+      sum-=candidates[i];
+    }
+
+  }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+      vector<vector<int>>ans;
+      vector<int>store;
+      int sum = 0;
+      int index = 0;
+      solve(candidates,target,ans,store,sum,index);
+      return ans;
+        
+    }
+};
